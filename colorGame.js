bfbc9385379380f24ba752128d2dd72dcb0195c8
Enoch Nordlander <50180679+enochnord@users.noreply.github.com>
2020-01-30 09:57:19 -0500

    Update colorGame.js

--- colorGame.js
+++ colorGame.js
@@ -8,14 +8,12 @@ var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
-var mediumBtn = document.querySelector("#mediumBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
 
 // easy button
 easyBtn.addEventListener("click", function() {
     easyBtn.classList.add("selected");
-    mediumBtn.classList.remove("selected");
     hardBtn.classList.remove("selected");
     numofSquares = 3;
     colors = generateColors(numofSquares);
@@ -30,29 +28,11 @@ easyBtn.addEventListener("click", function() {
     }
 });
 
-// medium button
-mediumBtn.addEventListener("click", function() {
-    mediumBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    hardBtn.classList.remove("selected");
-    numofSquares = 6;
-    colors = generateColors(numofSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(var i = 0; i < squares.length; i++) {
-        if(colors[i]) {
-            squares[i].style.background = colors[i];
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-});
 
 // hard button
 hardBtn.addEventListener("click", function() {
     hardBtn.classList.add("selected");
     easyBtn.classList.remove("selected");
-    mediumBtn.classList.remove("selected");
     numofSquares = 9;
     colors = generateColors(numofSquares);
     pickedColor = pickColor();
@@ -71,6 +51,9 @@ resetButton.addEventListener("click", function() {
     pickedColor = pickColor();
     // change colorDisplay to match picked color
     colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
+    // erase "correct" text when button is clicked
+    messageDisplay.textContent = "";
     // change colors of squares
     for(var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
@@ -90,13 +73,15 @@ for(var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         // compare color to pickedColor
         if(clickedColor === pickedColor) {
-            messageDisplay.textContent = "Correct";
+            messageDisplay.style.color = "green";
+            messageDisplay.textContent = "Correct!!!";
             resetButton.textContent = "Play Again?";
             changeColor(clickedColor);
             h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#000000";
-            messageDisplay.textContent = "Try Again";
+            messageDisplay.style.color = "red";
+            messageDisplay.textContent = "Try Again!!!";
         }
     });
 }
